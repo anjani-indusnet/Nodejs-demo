0471cd167c92e25877a2cebac96906d1aebcaa31
Anjani-Gummalla <anjani.gummalla@indusnet.co.in>
2023-02-09 21:10:05 +0530

    first commit

---
+++ OAuth2.0/server/src/config.ts
@@ -0,0 +1,6 @@
+export const GOOGLE_CLIENT_ID ="774158675875-04h5l7asf109q26t8p122bj1bbhkvr2s.apps.googleusercontent.com";
+export const GOOGLE_CLIENT_SECRET = "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";
+export const SERVER_ROOT_URI = "http://localhost:4000";
+export const UI_ROOT_URI = "http://localhost:3000";
+export const JWT_SECRET = "shhhhh";
+export const COOKIE_NAME = "auth_token";
